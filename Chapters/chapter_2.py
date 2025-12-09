0ee9cd4983af4aa06f6ed51527f6e11defb14007
didarkova <di.darkova@gmail.com>
2025-12-09 15:40:56 +0100

    starting the chapter 2

--- Chapters/chapter_2.py
+++ Chapters/chapter_2.py
@@ -1,3 +1,17 @@
 #Chapter 2: The Journey to Hogwarts
+from universe.characters import *
 
+character = init_character()
 def meet_friends(character):
+
+    print("Hi ! I'm Ron Weasley. Mind if I sit with you?")
+    print("How do you respond?")
+    print("1. Sure, have a seat!")
+    print("2. Sorry, I prefer to travel alone.")
+    choice = input("Your choice: ")
+
+    if choice == "1":
+        character["Attributes"]["loyalty"] = 1
+    else:
+        character["Attributes"]["ambition"] = 1
+
