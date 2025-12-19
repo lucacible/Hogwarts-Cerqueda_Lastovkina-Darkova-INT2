9c0ba3e7fc0c3133ddc89ee2b4b87f48c0005a31
didarkova <di.darkova@gmail.com>
2025-12-19 09:58:06 +0100

    Menu finished, main created and finished

---
+++ main.py
@@ -0,0 +1,4 @@
+#################### PLAY THE GAME ##################
+from menu import *
+if __name__ == "__main__":
+    launch_menu_choice()
--- menu.py
+++ menu.py
@@ -1,7 +1,27 @@
 #Python for the main menu of the game
 from utils.input_utils import ask_choice
-def display_main_menu():
-    print("")
+from universe.house import houses
+from Chapters.chapter_1 import start_chapter_1
+from Chapters.chapter_2 import start_chapter_2, character
+from Chapters.chapter_3 import start_chapter_3
 
-def launch_main_menu():
-    houses
+def display_main_menu():
+    main_game_menu = """
+    1. Start CHapter 1 - Arrival in the magical world.
+    2. Exit the game.
+    """
+
+def launch_menu_choice():
+    print(display_main_menu())
+    choice = ask_choice(display_main_menu(), ["1", "2"])
+    if choice == "1":
+        start_chapter_1()
+        start_chapter_2(character)
+        start_chapter_3(character, houses)
+        #start_chapter_4_quidditch(character, houses)
+    elif choice == "2":
+        print("Thank you for your attention! Hogwarts hopes to see you among its students in the future!")
+        exit()
+    else:
+        print("Please enter a valid option.")
+        launch_menu_choice()
